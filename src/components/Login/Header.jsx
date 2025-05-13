554c27512e6d1a8bb4497f4d270242fe041b5fdf
Leillac88 <leillacarvalho88@gmail.com>
2025-05-13 17:00:26 -0300

    add subtitle

--- src/components/Login/Header.jsx
+++ src/components/Login/Header.jsx
@@ -4,6 +4,7 @@ function Header() {
   return (
     <header className="login-header">
       <h1>Login</h1>
+      {/* <p className="subtitle">Acesse sua conta para continuar</p> */}
     </header>
   );
 }
